68760cee32fb8264662020dfc6a9690aad959b89
panther <kinuthiaduncan@gmail.com>
2018-03-20 16:52:43 +0300

    function templates

--- data_structures_practice/main.cpp
+++ data_structures_practice/main.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
+#include <vector>
 #include "IntCell.h"
 
 using namespace std;
 
-int main() {
-    IntCell a;
-    a.write(2);
-    cout<<a.read()<<endl;
-    return 0;
+template <typename Comparable>
 
+const Comparable & findMax( const vector<Comparable> & a )
+{
+    int maxIndex = 0;
+
+    for( int i = 1; i < a.size( ); ++i )
+        if( a[ maxIndex ] < a[ i ] )
+            maxIndex = i;
+
+    return a[ maxIndex ];
+}
+int main()
+{
+    vector<int> v1= {1,2,3,4,14,56,89,90,1,2,355};
+    cout<<findMax(v1)<<endl;
+    return 0;
 }
